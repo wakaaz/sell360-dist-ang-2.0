49d1165c41bc23a1377dd19a40109ea6ca893b3c
Zubaid.Dev <web.expert1221@gmail.com>
2021-07-26 11:31:11 +0500

    Product details API bindings

--- src/app/core/constants/api-urls.constants.ts
+++ src/app/core/constants/api-urls.constants.ts
@@ -20,7 +20,6 @@ export const API_URLS = {
     PRODUCT: '/product',
     DISTRIBUTOR_PURCHASE_DATA: '/distributorPurchaseData',
     ADD_DISTRIBUTOR_PURCHASE: '/addDistributorPurchase',
-    PRODUCT: '/product',
     PRODUCTS_WITH_STOCK: '/productsWithStock',
 
     // Retailers

f455c9f829ef6287f62d03f16c5da08ce87cf7f2
M Ahsan Hameed <ahsanjutt0999@outlook.com>
2022-08-30 11:59:11 +0500

    remove dist-api from reporting url

--- src/app/modules/retailer/components/ledger/ledger.component.ts
+++ src/app/modules/retailer/components/ledger/ledger.component.ts
@@ -7,17 +7,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ledger',
   templateUrl: './ledger.component.html',
-  styleUrls: ['./ledger.component.css']
+  styleUrls: ['./ledger.component.css'],
 })
 export class LedgerComponent implements OnInit {
-
   filter = {
     orderBooker: null,
     route: null,
     segment: null,
     startDate: null,
     endDate: null,
-  }
+  };
   retailerCore: any;
   loading: boolean;
 
@@ -26,12 +25,12 @@ export class LedgerComponent implements OnInit {
   constructor(
     private retailerService: RetailerService,
     private toastService: ToasterService,
-    private storageService: LocalStorageService,
-  ) { }
+    private storageService: LocalStorageService
+  ) {}
 
   ngOnInit(): void {
     // this.loading = true
-    this.retailerService.getRetailerCore().subscribe(data => {
+    this.retailerService.getRetailerCore().subscribe((data) => {
       this.retailerCore = data;
       // this.getRetailerListing();
     });
@@ -41,34 +40,57 @@ export class LedgerComponent implements OnInit {
 
   filterRetailers() {
     let params = '';
-    params = params + (this.filter.orderBooker ? `order_booker=${this.filter.orderBooker}` : params + '');
-    params = params.length ? (this.filter.route ? params + `&route=${this.filter.route}` : params + '')
-      : this.filter.route ? params + `route=${this.filter.route}` : params + '';
-    params = params.length ? (this.filter.segment ? params + `&segment=${this.filter.segment}` : params + '')
-      : this.filter.segment ? params + `segment=${this.filter.segment}` : params + '';
+    params =
+      params +
+      (this.filter.orderBooker
+        ? `order_booker=${this.filter.orderBooker}`
+        : params + '');
+    params = params.length
+      ? this.filter.route
+        ? params + `&route=${this.filter.route}`
+        : params + ''
+      : this.filter.route
+      ? params + `route=${this.filter.route}`
+      : params + '';
+    params = params.length
+      ? this.filter.segment
+        ? params + `&segment=${this.filter.segment}`
+        : params + ''
+      : this.filter.segment
+      ? params + `segment=${this.filter.segment}`
+      : params + '';
 
     this.getRetailerListing(params);
   }
 
   getRetailerListing(queryParams = null): void {
     this.loading = true;
-    this.retailerService.getRetailerListing(queryParams).subscribe(res => {
+    this.retailerService.getRetailerListing(queryParams).subscribe((res) => {
       this.loading = false;
       this.retailers = res;
     });
   }
 
   generateLedger() {
-    if (!this.retailerSelected || !this.filter.startDate || !this.filter.endDate) {
+    if (
+      !this.retailerSelected ||
+      !this.filter.startDate ||
+      !this.filter.endDate
+    ) {
       return this.toastService.showToaster({
         title: 'Error:',
         message: 'Please select a retailer & provide start/end dates',
-        type: 'error'
+        type: 'error',
       });
     }
 
-    window.open(`${environment.apiDomain}/distributor-api/retailerBalanceSheetPdf/${this.retailerSelected}/${this.filter.startDate}/${this.filter.endDate}?session=${this.storageService.getItem('dist_session')}`, "_blank");
-
+    window.open(
+      `${environment.apiDomain}/retailerBalanceSheetPdf/${
+        this.retailerSelected
+      }/${this.filter.startDate}/${
+        this.filter.endDate
+      }?session=${this.storageService.getItem('dist_session')}`,
+      '_blank'
+    );
   }
-
 }

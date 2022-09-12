8409cfba27f04f3a088817eafaaafa117294b21a
M Ahsan Hameed <ahsanjutt0999@outlook.com>
2022-09-12 15:13:03 +0500

    discount permission bug fix

--- src/app/modules/shared/components/order-items/order-items.component.ts
+++ src/app/modules/shared/components/order-items/order-items.component.ts
@@ -1,4 +1,15 @@
-import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+  ViewEncapsulation,
+} from '@angular/core';
+import { localStorageKeys } from 'src/app/core/constants/localstorage.constants';
+import { LocalStorageService } from 'src/app/core/services/storage.service';
 import { Toaster, ToasterService } from 'src/app/core/services/toaster.service';
 import { PaymentDetail } from 'src/app/modules/orders/models/counter-sale.model';
 import { DataService } from '../../services';
@@ -8,9 +19,7 @@ import { DataService } from '../../services';
   templateUrl: 'order-items.component.html',
   styleUrls: ['./order-items.component.css'],
 })
-
 export class OrderItemsListComponent implements OnInit, OnChanges {
-
   @Input() orderType: string;
   @Input() returnAmount: number;
   @Input() isChequeAdded: boolean;
@@ -45,6 +54,7 @@ export class OrderItemsListComponent implements OnInit, OnChanges {
   totalTax: number;
 
   selectedItem: any;
+  permissions: any;
 
   @Output() openDrawer: EventEmitter<boolean> = new EventEmitter();
   @Output() openReturnedModal: EventEmitter<boolean> = new EventEmitter();
@@ -56,8 +66,12 @@ export class OrderItemsListComponent implements OnInit, OnChanges {
   constructor(
     private toastService: ToasterService,
     private dataService: DataService,
+    private storageService: LocalStorageService
   ) {
     this.resetValues();
+    this.permissions = this.storageService.getItem(
+      localStorageKeys.permissions
+    );
   }
 
   ngOnInit(): void {
@@ -96,7 +110,7 @@ export class OrderItemsListComponent implements OnInit, OnChanges {
       this.toastService.showToaster({
         type: 'error',
         title: 'Error:',
-        message: 'Please select order to add products!'
+        message: 'Please select order to add products!',
       });
     }
   }
@@ -109,7 +123,7 @@ export class OrderItemsListComponent implements OnInit, OnChanges {
       this.toastService.showToaster({
         type: 'error',
         title: 'Error:',
-        message: 'Please select order to add returned products!'
+        message: 'Please select order to add returned products!',
       });
     }
   }
@@ -128,7 +142,9 @@ export class OrderItemsListComponent implements OnInit, OnChanges {
         this.selectedItem.isDeleted = true;
         // this.selectedItem.item_status = 0;
       } else {
-        this.orderDetail.items = this.orderDetail.items.filter(x => x.item_id !== this.selectedItem.item_id);
+        this.orderDetail.items = this.orderDetail.items.filter(
+          (x) => x.item_id !== this.selectedItem.item_id
+        );
         this.grossAmount = this.grossAmount - this.selectedItem.original_amount;
         this.applySlabOnAllProducts();
       }
@@ -142,29 +158,45 @@ export class OrderItemsListComponent implements OnInit, OnChanges {
       product.stockQty = 0;
     }
     if (this.orderType === 'execution') {
-      const prod = this.allProducts.find(x => x.item_id === product.item_id);
-      if ((product.id && +product.stockQty > (prod.available_qty + product.executed_qty)) ||
-        (!product.id && +product.stockQty > prod.available_qty)) {
+      const prod = this.allProducts.find((x) => x.item_id === product.item_id);
+      if (
+        (product.id &&
+          +product.stockQty > prod.available_qty + product.executed_qty) ||
+        (!product.id && +product.stockQty > prod.available_qty)
+      ) {
         const toast: Toaster = {
-          message: 'Executed quantity cannot be greater than available quantity!', type: 'error',
-          title: 'Quantity Error:'
+          message:
+            'Executed quantity cannot be greater than available quantity!',
+          type: 'error',
+          title: 'Quantity Error:',
         };
         this.toastService.showToaster(toast);
         product.stockQty = 0;
       }
     }
     if (product.item_trade_price) {
-      if (this.orderType !== 'execution' && product.item_quantity_booker > 0 && +product.stockQty === 0) {
+      if (
+        this.orderType !== 'execution' &&
+        product.item_quantity_booker > 0 &&
+        +product.stockQty === 0
+      ) {
         product.isDeleted = true;
-      } else if (this.orderType === 'execution' && product.dispatch_qty > 0 && +product.stockQty === 0) {
+      } else if (
+        this.orderType === 'execution' &&
+        product.dispatch_qty > 0 &&
+        +product.stockQty === 0
+      ) {
         product.isDeleted = true;
       } else {
         product.isDeleted = false;
       }
-      if (this.orderDetail.items.find(x => x.item_id === product.item_id)) {
+      if (this.orderDetail.items.find((x) => x.item_id === product.item_id)) {
         this.grossAmount = this.grossAmount - product.original_amount;
       }
-      product.parent_qty_sold = this.dataService.getParentQty(+product.stockQty, product.parent_quantity || product.quantity);
+      product.parent_qty_sold = this.dataService.getParentQty(
+        +product.stockQty,
+        product.parent_quantity || product.quantity
+      );
       this.calculateProductDiscounts(product);
       this.calculateProductPrice(product);
       this.calculateTotalBill();
@@ -174,13 +206,19 @@ export class OrderItemsListComponent implements OnInit, OnChanges {
   }
 
   setReturnedQty(product: any): void {
-    const productAvalableQty = this.allProducts.find(x => x.item_id === product.item_id)?.available_qty;
-    if (productAvalableQty < product.quantity_returned && +product.stockQty < product.quantity_returned &&
-      product.return_type !== 'damage') {
+    const productAvalableQty = this.allProducts.find(
+      (x) => x.item_id === product.item_id
+    )?.available_qty;
+    if (
+      productAvalableQty < product.quantity_returned &&
+      +product.stockQty < product.quantity_returned &&
+      product.return_type !== 'damage'
+    ) {
       this.toastService.showToaster({
         title: 'Returned Product:',
-        message: 'The selected product is part of other order please remove from other orders to update the quantity!',
-        type: 'error'
+        message:
+          'The selected product is part of other order please remove from other orders to update the quantity!',
+        type: 'error',
       });
       product.stockQty = product.quantity_returned;
       return;
@@ -190,14 +228,17 @@ export class OrderItemsListComponent implements OnInit, OnChanges {
     }
     product.gross_amount = -(product.item_trade_price * product.stockQty);
     product.original_amount = -(product.item_trade_price * product.stockQty);
-    product.total_retail_price = -(product.item_retail_price * product.stockQty);
+    product.total_retail_price = -(
+      product.item_retail_price * product.stockQty
+    );
     if (product.extra_discount) {
       product.booker_discount = +product.extra_discount;
-      product.unit_price_after_individual_discount = product.item_trade_price
-        - product.booker_discount;
+      product.unit_price_after_individual_discount =
+        product.item_trade_price - product.booker_discount;
     }
-    product.net_amount = -(product.unit_price_after_individual_discount
-      * product.stockQty);
+    product.net_amount = -(
+      product.unit_price_after_individual_discount * product.stockQty
+    );
     this.productUpdated.emit();
   }
 
@@ -205,17 +246,21 @@ export class OrderItemsListComponent implements OnInit, OnChanges {
     if (product.stockQty) {
       if (this.checkDiscount(product)) {
         product.booker_discount = +product.extra_discount;
-        product.unit_price_after_individual_discount = product.item_trade_price
-          - product.booker_discount;
-        product.net_amount = -(product.unit_price_after_individual_discount
-          * product.stockQty);
+        product.unit_price_after_individual_discount =
+          product.item_trade_price - product.booker_discount;
+        product.net_amount = -(
+          product.unit_price_after_individual_discount * product.stockQty
+        );
         this.productUpdated.emit();
       }
     }
   }
 
   checkDiscount(product: any): boolean {
-    if (product.extra_discount && +product.extra_discount > product.item_trade_price) {
+    if (
+      product.extra_discount &&
+      +product.extra_discount > product.item_trade_price
+    ) {
       this.resetDiscount(product);
       return false;
     }
@@ -224,8 +269,9 @@ export class OrderItemsListComponent implements OnInit, OnChanges {
 
   resetDiscount(product: any): void {
     const toast: Toaster = {
-      type: 'error', title: 'Incorrect discount:',
-      message: `Discount should not be greater than item price!`
+      type: 'error',
+      title: 'Incorrect discount:',
+      message: `Discount should not be greater than item price!`,
     };
     this.toastService.showToaster(toast);
     product.extra_discount = 0;
@@ -233,12 +279,20 @@ export class OrderItemsListComponent implements OnInit, OnChanges {
   }
 
   applySlabOnAllProducts(): void {
-    if (this.merchantDiscount && this.merchantDiscount.discount_filter === 'slab') {
-      this.orderDetail.items = this.orderDetail.items.map(product => {
-        product = this.dataService.applySlabForTotal(product, this.merchantDiscount, this.grossAmount);
+    if (
+      this.merchantDiscount &&
+      this.merchantDiscount.discount_filter === 'slab'
+    ) {
+      this.orderDetail.items = this.orderDetail.items.map((product) => {
+        product = this.dataService.applySlabForTotal(
+          product,
+          this.merchantDiscount,
+          this.grossAmount
+        );
         product = this.calculateProductSpecialDiscount(product);
         if (product.extra_discount) {
-          product.price = product.unit_price_after_special_discount - +product.extra_discount;
+          product.price =
+            product.unit_price_after_special_discount - +product.extra_discount;
         }
         this.calculateNetAmountOfProduct(product);
         return product;
@@ -248,9 +302,12 @@ export class OrderItemsListComponent implements OnInit, OnChanges {
   }
 
   calculateProductPrice(product): void {
-    product.original_amount = this.dataService.calculateUnitPrice(+product.stockQty,
-      product.item_trade_price);
-    product.gross_amount = product.unit_price_after_scheme_discount * +product.stockQty;
+    product.original_amount = this.dataService.calculateUnitPrice(
+      +product.stockQty,
+      product.item_trade_price
+    );
+    product.gross_amount =
+      product.unit_price_after_scheme_discount * +product.stockQty;
   }
 
   calculateProductDiscounts(product: any, scheme?: any): void {
@@ -263,16 +320,24 @@ export class OrderItemsListComponent implements OnInit, OnChanges {
     } else {
       product.scheme_discount = 0;
       product.price = JSON.parse(JSON.stringify(product.item_trade_price));
-      product.unit_price_after_scheme_discount = JSON.parse(JSON.stringify(product.item_trade_price));
+      product.unit_price_after_scheme_discount = JSON.parse(
+        JSON.stringify(product.item_trade_price)
+      );
     }
 
     // Trade Discount
     if (this.merchantDiscount) {
-      product = this.dataService.applyMerchantDiscountForSingleProduct(this.merchantDiscount, product, this.grossAmount);
+      product = this.dataService.applyMerchantDiscountForSingleProduct(
+        this.merchantDiscount,
+        product,
+        this.grossAmount
+      );
     } else {
       product.trade_discount = 0;
       product.trade_discount_pkr = 0;
-      product.unit_price_after_merchant_discount = JSON.parse(JSON.stringify(product.price));
+      product.unit_price_after_merchant_discount = JSON.parse(
+        JSON.stringify(product.price)
+      );
     }
 
     // Special Discount
@@ -286,23 +351,31 @@ export class OrderItemsListComponent implements OnInit, OnChanges {
     } else {
       this.calculateExtraDiscount(product);
     }
-
   }
 
   calculateProductSpecialDiscount(product: any): any {
-    return this.dataService.getSpecialDiscounts(this.selectedRetailer.segment_id,
-      this.selectedRetailer.region_id, product, this.specialDiscounts);
+    return this.dataService.getSpecialDiscounts(
+      this.selectedRetailer.segment_id,
+      this.selectedRetailer.region_id,
+      product,
+      this.specialDiscounts
+    );
   }
 
   calculateExtraDiscount(product: any): void {
     if (+product.extra_discount < product.unit_price_after_special_discount) {
-      product.price = product.unit_price_after_special_discount - +product.extra_discount;
+      product.price =
+        product.unit_price_after_special_discount - +product.extra_discount;
       product.extra_discount_pkr = +product.stockQty * +product.extra_discount;
     } else {
       product.extra_discount = 0;
       product.extra_discount_pkr = 0;
       product.price = product.unit_price_after_special_discount;
-      const toast: Toaster = { type: 'error', message: 'Discount should not be greater than item price!', title: 'Error:' };
+      const toast: Toaster = {
+        type: 'error',
+        message: 'Discount should not be greater than item price!',
+        title: 'Error:',
+      };
       this.toastService.showToaster(toast);
     }
     this.calculateNetAmountOfProduct(product);
@@ -311,13 +384,17 @@ export class OrderItemsListComponent implements OnInit, OnChanges {
   }
 
   calculateNetAmountOfProduct(product: any): any {
-    product.net_amount = this.dataService.calculateUnitPrice(product.price, +product.stockQty);
+    product.net_amount = this.dataService.calculateUnitPrice(
+      product.price,
+      +product.stockQty
+    );
     this.calculateProductTax(product);
   }
 
   calculateProductTax(product: any): void {
     if (product.tax_class_amount) {
-      product.tax_amount_value = (product.tax_class_amount / 100) * product.item_retail_price;
+      product.tax_amount_value =
+        (product.tax_class_amount / 100) * product.item_retail_price;
       product.tax_amount_pkr = product.tax_amount_value * product.stockQty;
       product.net_amount = product.net_amount + product.tax_amount_pkr;
     } else {
@@ -359,7 +436,7 @@ export class OrderItemsListComponent implements OnInit, OnChanges {
       this.toastService.showToaster({
         title: 'Recovery Error:',
         message: `The recovered amount cannot be greater than the credit (${this.recoveryAmount})`,
-        type: 'error'
+        type: 'error',
       });
       this.orderDetail.recovered = 0;
     }
@@ -369,35 +446,51 @@ export class OrderItemsListComponent implements OnInit, OnChanges {
   calculateTotalBill(): void {
     if (this.orderDetail.items.length) {
       this.selectedProductQuantities = this.orderDetail.items
-        .map(product => +product.parent_qty_sold).reduce((a, b) => a + b);
+        .map((product) => +product.parent_qty_sold)
+        .reduce((a, b) => a + b);
     }
     // Gross Amount
-    let prices = this.orderDetail.items.map(product => product.original_amount);
+    let prices = this.orderDetail.items.map(
+      (product) => product.original_amount
+    );
     this.grossAmount = this.dataService.calculateItemsBill(prices);
     // Net Amount
-    prices = this.orderDetail.items.map(product => product.net_amount);
+    prices = this.orderDetail.items.map((product) => product.net_amount);
     this.netAmount = this.dataService.calculateItemsBill(prices);
     if (this.selectedRetailer && this.orderType !== 'execution') {
       // this.selectedRetailer.order_total = this.netAmount;
-      this.orders.find(x => x.id === this.selectedRetailer.id).order_total = this.netAmount;
+      this.orders.find((x) => x.id === this.selectedRetailer.id).order_total =
+        this.netAmount;
     }
     // Total Retail Price
-    prices = this.orderDetail.items.map(product => +product.stockQty * product.item_retail_price);
+    prices = this.orderDetail.items.map(
+      (product) => +product.stockQty * product.item_retail_price
+    );
     const totalRetailPrice = this.dataService.calculateItemsBill(prices);
     // Scheme Discount
-    let discount = this.orderDetail.items.map(product => (+product.stockQty * product.scheme_discount));
+    let discount = this.orderDetail.items.map(
+      (product) => +product.stockQty * product.scheme_discount
+    );
     this.totalSchemeDiscount = this.dataService.calculateItemsBill(discount);
     // Trade Discount
-    discount = this.orderDetail.items.map(product => (+product.stockQty * product.trade_discount_pkr));
+    discount = this.orderDetail.items.map(
+      (product) => +product.stockQty * product.trade_discount_pkr
+    );
     this.totalMerchantDiscount = this.dataService.calculateItemsBill(discount);
     // Special Discount
-    discount = this.orderDetail.items.map(product => +product.stockQty * product.special_discount_pkr);
+    discount = this.orderDetail.items.map(
+      (product) => +product.stockQty * product.special_discount_pkr
+    );
     this.totalSpecialDiscount = this.dataService.calculateItemsBill(discount);
     // Extra Discount
-    discount = this.orderDetail.items.map(product => +product.extra_discount_pkr);
+    discount = this.orderDetail.items.map(
+      (product) => +product.extra_discount_pkr
+    );
     this.totalBookerDiscount = this.dataService.calculateItemsBill(discount);
     // Tax
-    const taxes = this.orderDetail.items.map(product => product.tax_amount_pkr);
+    const taxes = this.orderDetail.items.map(
+      (product) => product.tax_amount_pkr
+    );
     this.totalTax = this.dataService.calculateItemsBill(taxes);
     this.orderDetail.total_amount_after_tax = this.netAmount;
     if (this.orderType === 'execution') {
@@ -405,9 +498,11 @@ export class OrderItemsListComponent implements OnInit, OnChanges {
         // this.selectedRetailer.order_total = this.totalPayment;
         let order;
         if (this.currentTab === 1) {
-          order = this.orders.find(x => x.id === this.selectedRetailer.id);
+          order = this.orders.find((x) => x.id === this.selectedRetailer.id);
         } else {
-          order = this.orders.find(x => x.retailer_id === this.selectedRetailer.retailer_id);
+          order = this.orders.find(
+            (x) => x.retailer_id === this.selectedRetailer.retailer_id
+          );
         }
         if (order) {
           order.order_total = this.dueAmount;
@@ -417,7 +512,8 @@ export class OrderItemsListComponent implements OnInit, OnChanges {
       this.orderDetail.total_amount_after_tax = this.dueAmount;
     }
 
-    this.orderDetail.total_discount = this.totalSchemeDiscount +
+    this.orderDetail.total_discount =
+      this.totalSchemeDiscount +
       this.totalMerchantDiscount +
       this.totalSpecialDiscount +
       this.totalBookerDiscount;

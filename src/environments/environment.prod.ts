0673aac10acedbe4f3c210fe44f8476c8abfa71a
M Ahsan Hameed <ahsanjutt0999@outlook.com>
2022-09-15 17:06:29 +0500

    changed base url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiDomain: 'https://rocket.sell360.app',
-  apiUrl: 'https://rocket.sell360.app/distributor-api',
+  apiDomain: ' https://marketer.sell360.app',
+  apiUrl: ' https://marketer.sell360.app/distributor-api',
   DISCOUNT_TYPES: {
     NORMAL: 1, // noramal means flat value * qty
     PERCENTAGE: 2, // Percentage means (value/100) * qty
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: false,
   // apiDomain: 'https://shahi.sell360.app',
   // apiUrl: 'https://shahi.sell360.app/distributor-api',
-  apiDomain: 'https://rocket.sell360.app',
-  apiUrl: 'https://rocket.sell360.app/distributor-api',
+  apiDomain: ' https://marketer.sell360.app',
+  apiUrl: ' https://marketer.sell360.app/distributor-api',
 
   DISCOUNT_TYPES: {
     NORMAL: 1, // noramal means flat value * qty

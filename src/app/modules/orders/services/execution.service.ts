1da39b72404be79875d6e88270bfd57701220412
danial-allomate <danial@allomate.com>
2023-01-31 17:13:12 +0500

    dispatch issue is foxed

--- src/app/modules/orders/services/execution.service.ts
+++ src/app/modules/orders/services/execution.service.ts
@@ -87,10 +87,11 @@ export class ExecutionService {
       let finalQty            =   stockQty+free_qty;
 
       let ttl_scheme_discount =   item.scheme_id && item.scheme_type == 'bundle_offer' ? +item.scheme_discount: +(stockQty * item.scheme_discount) ;
-      let ttl_trade_discount  =  + stockQty * item.trade_discount_pkr;
-      let ttl_special_discount=  + stockQty * item.special_discount ? +item.special_discount:0;
-      let ttl_extra_discount  =  + item.extra_discount_pkr ? +item.extra_discount_pkr : 0;
-      let total_discount      =   ttl_scheme_discount + ttl_trade_discount + ttl_special_discount + ttl_extra_discount + ttl_extra_discount;
+      let ttl_trade_discount  =   +stockQty * item.trade_discount_pkr;
+      let ttl_special_discount=   item.special_discount ? stockQty * +item.special_discount:0;
+      let ttl_extra_discount  =   +item.extra_discount_pkr ? +item.extra_discount_pkr : 0;
+      let ttl_loyalty_discount=   item.loyalty_offer_discount_pkr ? +stockQty * +item.loyalty_offer_discount_pkr : 0;
+      let total_discount      =   ttl_scheme_discount + ttl_trade_discount + ttl_special_discount + ttl_extra_discount + ttl_loyalty_discount;
       let final_price         =   gross_sale_amount - total_discount;                          
       let tax_in_value        =   0;                          
       let total_tax_amount    =   0;    
@@ -130,7 +131,7 @@ export class ExecutionService {
         unit_price_after_merchant_discount: item.unit_price_after_merchant_discount,
         special_discount: item.special_discount,
         unit_price_after_special_discount:item.unit_price_after_special_discount,
-        booker_discount: item.extra_discount_pkr ? +item.extra_discount_pkr : 0,
+        booker_discount: item.extra_discount ? +item.extra_discount : 0,
         unit_price_after_individual_discount:item.unit_price_after_individual_discount || item.price,
         schemeitems:item.schemeitems ? item.schemeitems :null,
         parent_pref_id: item.child,

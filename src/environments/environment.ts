63a6c9ffa93e1d311869beac25f6565b77530d36
danial-allomate <danial@allomate.com>
2023-04-11 14:24:41 +0500

    updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,6 +9,9 @@ export const environment = {
   // apiDomain: 'https://asg.sell360.app', 
   // apiUrl: 'https://asg.sell360.app/distributor-api',
 
+  //  apiDomain: 'https://shahi-revamp.sell360.app', 
+  // apiUrl: 'https://shahi-revamp.sell360.app/distributor-api',   
+
   // apiDomain: ' https://staging.spencer.allomate.solutions',
   // apiUrl: ' https://staging.spencer.allomate.solutions/distributor-api',
   DISCOUNT_TYPES: {

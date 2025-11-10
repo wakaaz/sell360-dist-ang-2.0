35d4cdb245e6e5a5cf995a2ab4fb415f00e775de
irfannabiwebart <irfannabi.webart@gmail.com>
2025-11-10 18:13:54 +0500

    primary sale order page

--- src/app/modules/orders/primary-orders/primary-orders.module.ts
+++ src/app/modules/orders/primary-orders/primary-orders.module.ts
@@ -5,6 +5,7 @@ import { PrimaryOrdersRoutingModule } from './primary-orders-routing.module';
 import { ListComponent } from './components/list/list.component';
 // import { SharedModule } from '../shared/shared.module';
 import { NgSelectModule } from '@ng-select/ng-select';
+import { NzSelectModule } from 'ng-zorro-antd/select';
 import { FormsModule } from '@angular/forms';
 import { DataTablesModule } from 'angular-datatables';
 import { PrimaryOrdersService } from './services/primary-orders.service';
@@ -25,6 +26,7 @@ import { SharedModule } from '../../shared/shared.module';
     DataTablesModule,
     SharedModule,
     NgSelectModule,
+    NzSelectModule,
   ],
   providers: [PrimaryOrdersService, OrdersService],
 })

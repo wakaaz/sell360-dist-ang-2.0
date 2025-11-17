28327bdaa322b5066605a44a8ff9f330af6d7953
adeen allomate <adeen@allomate.com>
2025-11-17 12:38:42 +0500

    enabled complete for executed status

--- src/app/modules/orders/primary-orders/components/list/list.component.ts
+++ src/app/modules/orders/primary-orders/components/list/list.component.ts
@@ -105,7 +105,7 @@ export class ListComponent {
             `;
           }
           
-          if (PRIMARY_ORDER_CONST.EXECUTE !== orderStatus) {
+          if (PRIMARY_ORDER_CONST.EXECUTE === orderStatus) {
             buttons += `
               <button onclick="window.completeOrderClick('${order.id}')" 
                       class="bg-transparent h-auto leading-none py-[4px] px-[5px] text-primary text-[11px] border border-primary hover:bg-primary hover:text-white font-primary rounded-[5px]" title="Complete">Complete</button>
